574deb65e2f8099f2a48839abe2564c3dbf3b694
Ozzy <milkywaydebugger@gmail.com>
2021-08-10 01:28:21 -0400

    Initial Commit, Background gradient animation

---
+++ app/src/main/java/com/example/counterapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.counterapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ConstraintLayout constraintLayout= findViewById(R.id.mainLayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+    }
+}
